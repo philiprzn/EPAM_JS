b3b4e3a3d19672ce201e8d8e5fcdbfd752f76b06
philiprzn <philip_bmx11@yahoo.com>
2018-03-09 23:17:00 +0300

    js_task2_05:

--- main.js
+++ main.js
@@ -39,14 +39,45 @@
 
 //////////////////////////////////////////// js_task2_04:
 
-var arr=[1, 1, 1, 1];
-
-var newArr = arr.filter(function (item) {
-   return arr[item] === arr[0];
-});
-
-if (arr.length === newArr.length){
-    console.log(true);
-} else (console.log(false));
+// var arr=[1, 1, 1, 1];
+//
+// var newArr = arr.filter(function (item) {
+//    return arr[item] === arr[0];
+// });
+//
+// if (arr.length === newArr.length){
+//     console.log(true);
+// } else (console.log(false));
 
 // console.log(newArr);
+
+
+//////////////////////////////////////////// js_task2_05:
+
+// var numArr = [1, 83, 76];
+//
+// function getMaxOfArray(numArray) {
+//     return Math.max.apply(null, numArray);
+// }
+//
+// console.log(getMaxOfArray(numArr));
+
+//////////////////////////////////////////// js_task2_05_v2:
+
+var numArr = [1, 83, 76, 522, -25, 8452];
+
+
+
+min = numArr[0];
+max = min;
+
+numArr.forEach(function (item, i) {
+    // console.log(item);
+    if (numArr[i] > max) max = numArr[i];
+    if (numArr[i] < min) min = numArr[i];
+});
+
+console.log(max);
+console.log(min);
+
+
